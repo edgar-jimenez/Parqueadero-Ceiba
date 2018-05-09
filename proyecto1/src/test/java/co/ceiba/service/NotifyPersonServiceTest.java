0775de300e9c7fc573c41f5820e300267c8cc527
edgar-jimenez <39103097+edgar-jimenez@users.noreply.github.com>
2018-05-09 16:55:16 -0500

    subida

---
+++ proyecto1/src/test/java/co/ceiba/service/NotifyPersonServiceTest.java
@@ -0,0 +1,35 @@
+package co.ceiba.service;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import co.ceiba.domain.Person;
+import co.ceiba.testdatabuilder.PersonTestDataBuilder;
+
+public class NotifyPersonServiceTest {
+
+	private NotifyPersonService notifyPersonService;
+	private EmailService emailService;
+	
+	@Before
+	public void setup() {
+		emailService = Mockito.mock(EmailService.class);
+		notifyPersonService = new NotifyPersonService(emailService);
+	}
+	
+	@Test
+	public void notifyTest() {
+		//Arrenge
+		Mockito.when(emailService.sendMail(Mockito.anyString())).thenReturn("Hola mundo");
+		Person person = new PersonTestDataBuilder().build();
+		//Act
+		String message = notifyPersonService.notify(person);
+		
+		//Assert
+		System.out.println(message);;
+		Assert.assertNotNull(message);
+	}
+	
+}
